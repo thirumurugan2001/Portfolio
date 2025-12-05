a1195a2193c25019bb0fb2774c78b0fc6dc17bba
VPearl-ai <riverpearlsolutions@gmail.com>
2025-12-05 12:33:15 +0530

    update the base url

--- src/component/ApiURL.jsx
+++ src/component/ApiURL.jsx
@@ -1,7 +1,7 @@
 
 // const BASE_URL ='http://localhost:8080';
 // const BASE_URL ='https://rag-using-singlestore-database-and-cohere.onrender.com';
-const BASE_URL ='https://rag-using-singlestore-database-and.vercel.app/';
+const BASE_URL ='https://rag-using-singlestore-database-and.vercel.app';
 // 
 
 

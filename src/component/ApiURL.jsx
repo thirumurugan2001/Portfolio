a398ed05dc90b7200f833a190e7cf8963a04f341
VPearl-ai <riverpearlsolutions@gmail.com>
2025-12-05 11:30:19 +0530

    update the render URL

--- src/component/ApiURL.jsx
+++ src/component/ApiURL.jsx
@@ -1,5 +1,7 @@
 
-const BASE_URL ='http://localhost:8080';
+// const BASE_URL ='http://localhost:8080';
+const BASE_URL ='https://rag-using-singlestore-database-and-cohere.onrender.com';
+
 
 export const API_URLS = {
   CHATBOT: {
